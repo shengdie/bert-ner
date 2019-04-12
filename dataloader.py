a521d1c2a4f427a1c6c8e846765de54fb8bbd57a
shengdie <shilishengdie@gmail.com>
2019-04-12 17:31:20 -0400

    init

---
+++ dataloader.py
@@ -0,0 +1,71 @@
+import torch
+import numpy as np
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler, random_split
+
+
+def process_data(raw_txts, raw_labels, vocab, max_len=120, pad='[PAD]', unk='[UNK]', cls='[CLS]', add_cls=False, classes=[0,1,2,3]):
+    id_pad = vocab[pad]
+    id_cls = vocab[cls]
+    id_unk = vocab[unk]
+    padded_txt_idx = torch.tensor([([id_cls] if add_cls else []) + [vocab[w] if w in vocab.keys() else id_unk for w in s] + [id_pad] * (max_len - len(s)) \
+                                 if len(s) < max_len else \
+                                 ([id_cls] if add_cls else [])  + [vocab[w] if w in vocab.keys() else id_unk for w in s[:max_len]]
+                                 for s in raw_txts]).long()
+    padded_labels = torch.tensor([([classes[0]] if add_cls else []) + list(l) + [classes[0]] * (max_len - len(l)) \
+                                  if len(l) < max_len else \
+                                  ([classes[0]] if add_cls else []) + list(l[:max_len])
+                                  for l in raw_labels]).long()
+    #padded_txt_idx = torch.tensor([np.pad([vocab[w] if w in vocab.keys() else id_unk for w in s], 
+    #                       (1, max_len - len(s)), 'constant', constant_values=(id_cls, id_pad)) \
+    #                for s in raw_txts])
+    #padded_labels = torch.tensor([np.pad(l, (1, max_len - len(l)), 'constant', constant_values=(classes[0], classes[0])) 
+    #                 for l in raw_labels])
+    att_masks = (padded_txt_idx != id_pad).long()
+    return padded_txt_idx, padded_labels, att_masks
+
+def load_data(train_data_path, test_data_path, vocab, 
+              batch_size=32, val_size = 0.1, train_size=None, classes=[0,1,2,3],
+              max_len=120, pad='[PAD]', unk='[UNK]', cls='[CLS]', add_cls=False):
+    data = np.load(train_data_path).item()
+    data_test = np.load(test_data_path).item()
+    train_txts = []
+    train_labels = []
+    temp_txts = []
+    temp_labels = []
+    for v in data.values():
+        for r in v.values():
+            #if 'txt' in r.keys():
+            train_txts.extend(r['txt'])
+            train_labels.extend(r['output'])
+    for v in data_test.values():
+        temp_txts.extend(r['txt'])
+        temp_labels.extend(r['output'])
+    
+    train_input, train_output, train_att_masks =  process_data(train_txts, train_labels, vocab, max_len=max_len,
+                                                                pad=pad, unk=unk, cls=cls, add_cls=add_cls, classes=classes)
+    temp_input, temp_output, temp_att_masks = process_data(temp_txts, temp_labels, vocab, max_len=max_len,
+                                                            pad=pad, unk=unk, cls=cls, add_cls=add_cls,classes=classes)
+    if train_size is None:
+        train_size = len(train_input)
+    #train_size= 1000 #len(train_input)
+    train_dataset = TensorDataset(train_input[:train_size], train_output[:train_size], train_att_masks[:train_size])
+    temp_dataset = TensorDataset(temp_input, temp_output, temp_att_masks)
+
+    #test_size = int(0.9 * temp_input.size()[0] )
+    #val_size = temp_input.size()[0] - test_size
+    if type(val_size) is float:
+        val_size = int(val_size * temp_input.size()[0])
+        test_size = temp_input.size()[0] - val_size
+    else:
+        val_size = 32
+        test_size = temp_input.size()[0] - val_size
+
+    test_dataset, val_dataset = random_split(temp_dataset, [test_size, val_size])
+
+    train_sampler = RandomSampler(train_dataset)
+    val_sampler = SequentialSampler(val_dataset)
+
+    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size)
+    val_dataloader = DataLoader(val_dataset, sampler=val_sampler, batch_size=batch_size)
+
+    return train_dataloader, val_dataloader
---
+++ model.py
@@ -0,0 +1,191 @@
+import math
+import time
+import argparse
+import torch
+import numpy as np
+from collections import OrderedDict
+from torch.optim import Adam
+from pytorch_pretrained_bert import BertForTokenClassification, BertAdam
+from pytorch_pretrained_bert import BertTokenizer, BertConfig
+
+from tqdm.auto import tqdm #, trange
+from help_func import f1_score
+from dataloader import load_data
+
+
+class model(object):
+    def __init__(self, config_json, bert_weight, num_class=4):
+        config = BertConfig.from_json_file(config_json)
+        self.b_model = BertForTokenClassification(config=config, num_labels=num_class)
+
+        tmp_d = torch.load(bert_weight, map_location='cpu')
+        
+        if len(tmp_d.keys()) > 201:
+            state_dict = OrderedDict()
+            for i in list(tmp_d.keys())[:199]:
+                state_dict[i] = tmp_d[i]
+            cls_weight = torch.Tensor(num_class, config.hidden_size)
+            cls_bias = torch.Tensor(num_class)
+            torch.nn.init.kaiming_uniform_(cls_weight, a=math.sqrt(5))
+            fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(cls_weight)
+            bound = 1 / math.sqrt(fan_in)
+            torch.nn.init.uniform_(cls_bias, -bound, bound)
+        
+            state_dict['classifier.weight'] = cls_weight
+            state_dict['classifier.bias'] = cls_bias
+        else:
+            state_dict = tmp_d
+        
+        self.b_model.load_state_dict(state_dict)
+        # default train the classifier only
+        self.full_trainning(False)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if torch.cuda.is_available():
+            self.b_model.cuda()
+
+        self.total_epoch = 0
+
+    def train(self, train_dataloader, val_dataloader, epochs, tags, save_weight_path=None):
+        for i in range(self.total_epoch, self.total_epoch + epochs):
+            # TRAIN loop
+            total_ep = epochs + self.total_epoch
+            print('* Epoch {}/{}'.format(i+1, total_ep))
+            start_time = time.time()
+            self.b_model.train()
+            tr_loss = 0
+            nb_tr_examples, nb_tr_steps = 0, 0
+            with tqdm(total=len(train_dataloader), desc="Trainning", bar_format="{l_bar}{bar} [ time left: {remaining} ]", leave=False) as pbar:
+                for step, batch in enumerate(train_dataloader):
+                    # add batch to gpu
+                    batch = tuple(t.to(self.device) for t in batch)
+                    b_input_ids, b_labels, b_input_mask = batch
+
+                    # forward pass
+                    loss = self.b_model(b_input_ids, token_type_ids=None,
+                                attention_mask=b_input_mask, labels=b_labels)
+                    # backward pass
+                    loss.backward()
+                    # track train loss
+                    tr_loss += loss.item()
+                    nb_tr_examples += b_input_ids.size(0)
+                    nb_tr_steps += 1
+                    # update parameters
+                    self.optimizer.step()
+                    self.b_model.zero_grad()
+                    pbar.update(1)
+
+            print("Train loss: {}".format(tr_loss/nb_tr_steps))
+            # VALIDATION on validation set
+            ret_dic = self.predict(val_dataloader, tags)
+
+            print("Validation loss: {}".format(ret_dic['loss']))
+            print("Validation Accuracy: {}".format(ret_dic['accuracy']))
+            print("F1-Score: {}".format(ret_dic['f1-score']))
+            print('- Time elasped: {:.5f} seconds\n'.format(time.time() - start_time))
+            self.total_epoch += 1
+
+    def predict(self, test_dataloader, tags):
+        self.b_model.eval()
+        eval_loss, eval_accuracy = 0, 0
+        nb_eval_steps, nb_eval_examples = 0, 0
+        predictions , true_labels = [], []
+        ret_dic = {}
+        for batch in test_dataloader:
+            batch = tuple(t.to(self.device) for t in batch)
+            b_input_ids, b_labels,b_input_mask = batch
+            
+            with torch.no_grad():
+                tmp_eval_loss = self.b_model(b_input_ids, token_type_ids=None,
+                                    attention_mask=b_input_mask, labels=b_labels)
+                logits = self.b_model(b_input_ids, token_type_ids=None,
+                            attention_mask=b_input_mask)
+            logits = logits.detach().cpu().numpy()
+            label_ids = b_labels.to('cpu').numpy()
+            pred_flat = np.argmax(logits, axis=2)
+            predictions.extend(pred_flat)
+            pred_flat = pred_flat.flatten()
+            true_labels.extend(label_ids)
+            labels_flat = label_ids.flatten()
+            
+            tmp_eval_accuracy = np.sum(pred_flat == labels_flat) / len(labels_flat)
+            
+            eval_loss += tmp_eval_loss.mean().item()
+            eval_accuracy += tmp_eval_accuracy
+            
+            nb_eval_examples += b_input_ids.size(0)
+            nb_eval_steps += 1
+        eval_loss = eval_loss/nb_eval_steps
+
+        pred_tags = [[tags[pi] for pi in p] for p in predictions]
+        valid_tags = [[tags[li] for li in l] for l in true_labels]
+
+        ret_dic['loss'] = eval_loss
+        ret_dic['accuracy'] = eval_accuracy/nb_eval_steps
+        ret_dic['f1-score'] = f1_score(valid_tags, pred_tags)
+        ret_dic['pred'] = np.array(predictions)
+        ret_dic['truth'] = np.array(true_labels)
+        
+        return ret_dic
+
+    def full_trainning(self, full_trainning):
+        """If full_training, then train the classifier + BERT, else only train the classifier
+        """
+
+        if full_trainning:
+            param_optimizer = list(self.b_model.named_parameters())
+            no_decay = ['bias', 'gamma', 'beta']
+            optimizer_grouped_parameters = [
+                {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+                'weight_decay_rate': 0.01},
+                {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+                'weight_decay_rate': 0.0}
+            ]
+        else:
+            param_optimizer = list(self.b_model.classifier.named_parameters()) 
+            optimizer_grouped_parameters = [{"params": [p for n, p in param_optimizer]}]
+        self.optimizer = Adam(optimizer_grouped_parameters, lr=3e-5)
+
+    def save(self, path):
+        torch.save(self.b_model.state_dict(), path)
+
+
+if __name__ == "__main__":
+    vocab_path = 'weights/pubmed_pmc_470k/vocab.txt'
+    bert_conf_path = 'weights/pubmed_pmc_470k/bert_config.json'
+    bert_weight= 'weights/pytorch_weight'
+    train_data_path = 'data/data_BI.npy'
+    test_data_path = 'data/test_data_BI.npy'
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--train-size', '-ts', dest='train_size', type=int, default=-1,
+                        help='train data size')
+    parser.add_argument('--val-size', '-vs', dest='val_size', default='0.1', #type=int,
+                        help='val data size')
+    parser.add_argument('--epochs', '-ep', dest='epochs', default=5, type=int,
+                        help='epochs')
+    parser.add_argument('--full-train', '-ft', dest='full_trainning', action='store_true',
+                        help='if fully train the whole model')                             
+
+    pargs = parser.parse_args()
+    train_size = None if pargs.train_size < 0 else pargs.train_size
+    val_size = eval(pargs.val_size)
+
+    classes = [0, 1, 2, 3, 5, 6, 7]
+    tags = ['O', 'B-E', 'B-P', 'B-T', 'I-E', 'I-P', 'I-T']
+    #classes = [tags['']]
+
+    idx2token = np.loadtxt(vocab_path, dtype='str')
+    vocab = {idx2token[i]:i for i in range(len(idx2token))}
+
+    train_dataloader, val_dataloader = load_data(train_data_path, test_data_path, vocab, classes=classes,
+                                                 train_size=train_size, val_size=val_size)
+    # new model
+    net = model(bert_conf_path, bert_weight, num_class=len(classes))
+    if pargs.full_trainning:
+        net.full_trainning(True)
+    
+    # train
+    net.train(train_dataloader, val_dataloader, pargs.epochs, tags)
+
+
+
